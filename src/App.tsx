341645c5ee8353f00f84ebfd1107c01a03919b19
HansKalsi <hans.kalsi@yahoo.com>
2023-06-17 22:09:20 +0100

    Basic website structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
-import './App.scss';
+import './scss/App.scss';
 
 const App: React.FC = () => {
   return (
-    <div className="container">
-      <h1>Welcome to Vending Machine Supplier</h1>
-      <p>We provide top-quality vending machines for your business needs.</p>
-      <button className="btn waves-effect waves-light" type="button">Contact Us</button>
+    <div>
+      {/* Header */}
+      <header>
+        {/* Add your header content here */}
+        <nav>
+          {/* Add navigation links or burger menu here */}
+        </nav>
+      </header>
+
+      {/* Main Content */}
+      <main>
+        {/* Add your main content here */}
+        <div className="container">
+          <h1>Welcome to Vending Machine Supplier</h1>
+          <p>We provide top-quality vending machines for your business needs.</p>
+          <button className="btn waves-effect waves-light" type="button">
+            Contact Us
+          </button>
+        </div>
+      </main>
+
+      {/* Shopping Cart */}
+      <aside>
+        {/* Add your shopping cart component here */}
+      </aside>
+
+      {/* Footer */}
+      <footer>
+        {/* Add your footer content here */}
+      </footer>
     </div>
   );
 };
